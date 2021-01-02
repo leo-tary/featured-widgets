5f8bcec942bfe686622fec8563ee86b5aadc4ab3
leo-tary <leo.tarun@gmail.com>
2021-01-02 19:21:21 +0530

    Added little link to connect to the actual content

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,60 +5,57 @@ import wikipedia from '../services/wikipedia';
 
 const Search = () => {
 	const [term, setTerm] = useState('');
-    const [ searchResults , setSearchResults ] = useState([]);
+	const [searchResults, setSearchResults] = useState([]);
 
 	const onTextChange = (event) => {
 		// console.log(event.target.value);
 		setTerm(event.target.value);
 	};
 
-    // console.log("Runs with every render");
+	// console.log("Runs with every render");
 
-    useEffect(() => {
-        // console.log("I Too Run With Every Render");
+	useEffect(() => {
+		// console.log("I Too Run With Every Render");
 
-        const search = async () => {
-            const response = await wikipedia.get("" , {
-                params:{
-                    srsearch:term
-                }
-            })
-            console.log(response.data.query.search);
-            // setSearchResults(response.data.query);
-            // if(response.data.query !== undefined) {
-            //     setSearchResults(response.data.query);
-            // }
-        }
-        if(term) {
-            search();
-        }
-        
-    } , [term])
+		const search = async () => {
+			const { data } = await wikipedia.get('', {
+				params: {
+					srsearch: term,
+				},
+			});
+			console.log(data.query.search);
+			setSearchResults(data.query.search);
+		};
+		if (term) {
+			search();
+		}
+	}, [term]);
 
-    const renderArticles = () => {
-        return searchResults.map((article) => {
-            return (
-                <div key={article.pageid}>
-                    <div>{article.title}</div>
-                </div>
-            )
-        })
-    }
+	const renderArticles = () => {
+		return searchResults.map((article) => {
+			return (
+				<div className="item" key={article.pageid}>
+					<div className="right floated content">
+                        <a href={`https://en.wikipedia.org?curid=${article.pageid}`} className="ui button">Go</a>
+                    </div>
+                    <div className="content">
+						<div className="header">{article.title}</div>
+						<span dangerouslySetInnerHTML={{ __html: article.snippet }}></span>
+					</div>
+				</div>
+			);
+		});
+	};
 
-    const displayResults = () => {
-
-        if(term === '') {
-            return "";
-        }else if(searchResults.length === 0) {
-
-            return "no results to display";
-
-        }else {
-            return renderArticles();
-        }
-
-    }
-
+	const displayResults = () => {
+		if (term === '') {
+			return '';
+		} else if (searchResults.length === 0) {
+			return 'no results to display';
+		} else {
+			return renderArticles();
+		}
+	};
 
 	return (
 		<div>
@@ -71,14 +68,11 @@ const Search = () => {
 						placeholder="Enter Search Term"
 						onChange={(event) => {
 							onTextChange(event);
-                        }}
-
+						}}
 					/>
 				</div>
 			</div>
-            {
-                displayResults()
-            }
+			<div className="ui celled list">{displayResults()}</div>
 		</div>
 	);
 };
